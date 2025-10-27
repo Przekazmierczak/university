4b7940b9e2a5b4fd9206577aeb3bdce711dedd36
Kaźmierczak Przemysław <kp15319>
2025-10-27 16:32:24 +0100

    Update Lab1

--- algorithm2/Lab1/main.cpp
+++ algorithm2/Lab1/main.cpp
@@ -26,13 +26,19 @@ struct Linked_list {
         dummy->prev = dummy;
     }
 
-    void addFront(T newNodeT) {
-        Node* newNode = new Node(newNodeT);
-
-        newNode->next = dummy->next;
-        newNode->prev = dummy;
-        newNode->next->prev = newNode;
-        dummy->next = newNode;
+    int addFront(T newNodeT) {
+        try {
+            Node* newNode = new Node(newNodeT);
+            newNode->next = dummy->next;
+            newNode->prev = dummy;
+            newNode->next->prev = newNode;
+            dummy->next = newNode;
+            size++;
+            return 1;
+        }
+        catch (const std::bad_alloc&) {
+            return 1;
+        }
     };
 
     void addBack(T newNodeT) {
@@ -42,6 +48,7 @@ struct Linked_list {
         newNode->next = dummy;
         newNode->prev->next = newNode;
         dummy->prev = newNode;
+        size++;
     };
 
     int removeFront() {
@@ -50,6 +57,7 @@ struct Linked_list {
             dummy->next = dummy->next->next;
             dummy->next->prev = dummy;
             delete toRemove;
+            size--;
             return 0;
         }
         return 1;
@@ -61,9 +69,22 @@ struct Linked_list {
             dummy->prev = dummy->prev->prev;
             dummy->prev->next = dummy;
             delete toRemove;
+            size--;
             return 0;
         }
         return 1;
+    }
+
+    T atIndex(int index) {
+        return nodeAtIndex(index)->val;
+    }
+
+    void changeAtIndex(int index, T newValue) {
+        nodeAtIndex(index)->val = newValue;
+    }
+
+    T& operator[](int index) {
+        return nodeAtIndex(index)->val;
     }
 
     void printList() {
@@ -74,7 +95,36 @@ struct Linked_list {
             std::cout << curr->val.field_1 << ",";
             curr = curr->next;
         }
-        std::cout << "]" << std::endl;
+        std::cout << "]" << " size: " << size << std::endl;
+    }
+
+    private:
+    Node* nodeAtIndex(int index) {
+        if (index >= size) {
+            throw std::out_of_range("Index out of range");
+        }
+
+        if (index < size / 2) {
+            return nodeAtIndexFront(index);
+        }
+        return nodeAtIndexBack(size - index - 1);
+    }
+
+    Node* nodeAtIndexFront(int index) {
+        Node* curr = dummy->next;
+        for (int i = 0; i < index; i++) {
+            curr = curr->next;
+        }
+        return curr;
+    }
+
+    Node* nodeAtIndexBack(int index) {
+        Node* curr = dummy->prev;
+        for (int i = 0; i < index; i++) {
+            curr = curr->prev;
+        }
+
+        return curr;
     }
 };
 
@@ -105,6 +155,10 @@ int main () {
     s3.field_1 = 3;
     s3.field_2 = 'a';
     ll->addBack(s3);
+
+    Some_object s4;
+    s4.field_1 = 4;
+    s4.field_2 = 'a';
 
     ll->printList();
     std::cout<<ll->removeFront()<<std::endl;
@@ -142,6 +196,15 @@ int main () {
     ll->printList();
     ll->addBack(s3);
     ll->printList();
+    ll->addFront(s2);
+    ll->printList();
 
+    (*ll)[1] = s4;
+    ll->printList();
+
+    std::cout << (*ll)[0].field_1 << std::endl;
+    std::cout << ll->atIndex(1).field_1 << std::endl;
+    std::cout << (*ll)[2].field_1 << std::endl;
+    std::cout << ll->atIndex(3).field_1 << std::endl;
     delete ll;
 }
