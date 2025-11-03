3562bed2bb22b9b75f62499afea2f6f24614ebea
Przekazmierczak <prze.kazmierczak@gmail.com>
2025-11-03 21:35:22 +0100

    Update Lab1

--- algorithm2/Lab1/main.cpp
+++ algorithm2/Lab1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 template <typename T>
 struct Linked_list {
@@ -14,14 +15,18 @@ struct Linked_list {
         }
     };
 
-    T dummyVal;
     Node* dummy;
     int size = 0;
 
     Linked_list() {
-        dummy = new Node(dummyVal);
+        dummy = new Node(T());
         dummy->next = dummy;
         dummy->prev = dummy;
+    }
+
+    ~Linked_list() {
+        clear();
+        delete dummy;
     }
 
     int addFront(T newNodeVal) {
@@ -39,15 +44,15 @@ struct Linked_list {
     }
 
     int removeFront() {
-        return removeNodeFront(dummy);
+        return removeNode(dummy->next);
     }
 
     int removeBack() {
-        return removeNodeBack(dummy);
+        return removeNode(dummy->prev);
     }
 
-    T& getFromIndex(int index) {
-        if (index >= size) {
+    T& at(int index) const {
+        if (index >= size || index < 0) {
             throw std::out_of_range("Index out of range");
         }
 
@@ -57,16 +62,16 @@ struct Linked_list {
         return getFromIndexBack(size - index - 1);
     }
 
-    void setToIndex(int index, T newValue) {
-        getFromIndex(index) = newValue;
+    void set(int index, T newValue) {
+        at(index) = newValue;
     }
 
     T& operator[](int index) {
-        return getFromIndex(index);
+        return at(index);
     }
 
     template <typename A, typename O>
-    T* find(A searched, int (*cmp)(A, O)) {
+    T* find(A searched, int (*cmp)(const A&, const O&)) const {
         Node* find = findNode(searched, cmp);
         
         if (find != nullptr) return &(find->val);
@@ -75,21 +80,18 @@ struct Linked_list {
     }
 
     template <typename A>
-    int remove(A searched, int (*cmp)(A, T)) {
-        Node* toRemove = findNode(searched, cmp);
+    int remove(A searched, int (*cmp)(const A&, const T&)) {
+        Node* node = findNode(searched, cmp);
 
-        if (toRemove != nullptr) {
-            toRemove->prev->next = toRemove->next;
-            toRemove->next->prev = toRemove->prev;
-            delete toRemove;
-            size--;
+        if (node != nullptr) {
+            removeNode(node);
             return 0;
         }
 
         return 1;
     }
 
-    int addInOrder(T newNodeVal, int (*cmp)(T, T)) {
+    int insertOrdered(T newNodeVal, int (*cmp)(const T&, const T&)) {
         Node* newNode = createNewNode(newNodeVal);
         if (newNode == nullptr) return 1;
 
@@ -101,15 +103,26 @@ struct Linked_list {
         return 0;
     };
 
-    void printList() {
-        Node* curr = dummy->next;
+    void clear() {
+        while (dummy->next != dummy) {
+            removeNode(dummy->next);
+        }
+    }
 
-        std::cout << "[";
+    std::string toString(std::string (*toStringObj)(const T&)) const {
+        Node* curr = dummy->next;
+        bool first = true;
+
+        std::string str = "[";
         while (curr != dummy) {
-            std::cout << curr->val.field_1 << ",";
+            if (curr != dummy->next) str+= ";";
+            str += toStringObj(curr->val);
             curr = curr->next;
         }
-        std::cout << "]" << " size: " << size << std::endl;
+
+        str+= "]";
+
+        return str;
     }
 
     private:
@@ -138,31 +151,18 @@ struct Linked_list {
         size++;
     }
 
-    int removeNodeFront(Node* node) {
-        if (node->next != dummy) {
-            Node* toRemove = node->next;
-            node->next = node->next->next;
-            node->next->prev = node;
-            delete toRemove;
+    int removeNode(Node* node) {
+        if (node != dummy) {
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+            delete node;
             size--;
             return 0;
         }
         return 1;
     }
 
-    int removeNodeBack(Node* node) {
-        if (node->prev != dummy) {
-            Node* toRemove = node->prev;
-            node->prev = node->prev->prev;
-            node->prev->next = node;
-            delete toRemove;
-            size--;
-            return 0;
-        }
-        return 1;
-    }
-
-    T& getFromIndexFront(int index) {
+    T& getFromIndexFront(int index) const {
         Node* curr = dummy->next;
         for (int i = 0; i < index; i++) {
             curr = curr->next;
@@ -170,7 +170,7 @@ struct Linked_list {
         return curr->val;
     }
 
-    T& getFromIndexBack(int index) {
+    T& getFromIndexBack(int index) const {
         Node* curr = dummy->prev;
         for (int i = 0; i < index; i++) {
             curr = curr->prev;
@@ -180,11 +180,11 @@ struct Linked_list {
     }
 
     template <typename A>
-    Node* findNode(A searched, int (*cmp)(A, T)) {
+    Node* findNode(A search, int (*cmp)(const A&, const T&)) const {
         Node* curr = dummy->next;
 
         while (curr != dummy) {
-            if (cmp(searched, curr->val) == 0) {
+            if (cmp(search, curr->val) == 0) {
                 return curr;
             }
             curr = curr->next;
@@ -199,20 +199,24 @@ struct Some_object {
     char field_2;
 };
 
-int compare1(int value, Some_object obj) {
+int compare1(const int& value, const Some_object& obj) {
     if (value == obj.field_1) {
         return 0;
     }
     return 1;
 }
 
-int compare2(Some_object new_obj, Some_object list_obj) {
+int compare2(const Some_object& new_obj, const Some_object& list_obj) {
     if (new_obj.field_1 > list_obj.field_1) return 1;
     if (new_obj.field_1 < list_obj.field_1) return -1;
     return 0;
 }
 
-int main () {
+std::string toStringObj(const Some_object& obj) {
+    return std::to_string(obj.field_1);
+}
+
+int main() {
     Linked_list <Some_object>* ll = new Linked_list <Some_object>();
 
     Some_object s0;
@@ -239,73 +243,76 @@ int main () {
     s4.field_1 = 4;
     s4.field_2 = 'a';
 
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     std::cout<<ll->removeFront()<<std::endl;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     std::cout<<ll->removeFront()<<std::endl;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     std::cout<<ll->removeFront()<<std::endl;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     std::cout<<ll->removeFront()<<std::endl;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     std::cout<<ll->removeFront()<<std::endl;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     
     ll->addBack(s0);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     ll->addBack(s1);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     ll->addFront(s3);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     
     std::cout<<ll->removeBack()<<std::endl;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     std::cout<<ll->removeBack()<<std::endl;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     std::cout<<ll->removeBack()<<std::endl;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     std::cout<<ll->removeBack()<<std::endl;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     std::cout<<ll->removeBack()<<std::endl;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
 
     ll->addFront(s0);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     ll->addFront(s1);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     ll->addBack(s3);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     ll->addFront(s2);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
 
     (*ll)[1] = s4;
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
 
     std::cout << (*ll)[0].field_1 << std::endl;
-    std::cout << ll->getFromIndex(1).field_1 << std::endl;
+    std::cout << ll->at(1).field_1 << std::endl;
     std::cout << (*ll)[2].field_1 << std::endl;
-    std::cout << ll->getFromIndex(3).field_1 << std::endl;
+    std::cout << ll->at(3).field_1 << std::endl;
 
     std::cout << ll->find(4, compare1)->field_1 << std::endl;
     std::cout << ll->find(2, compare1)->field_1 << std::endl;
 
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     ll->remove(4, compare1);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     ll->remove(2, compare1);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     ll->remove(3, compare1);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
     ll->remove(0, compare1);
-    ll->printList();
+    std::cout << ll->toString(toStringObj) << std::endl;
 
-    ll->addInOrder(s2, compare2);
-    ll->addInOrder(s0, compare2);
-    ll->addInOrder(s4, compare2);
-    ll->addInOrder(s3, compare2);
-    ll->addInOrder(s1, compare2);
-    //ll->addInOrder(s0, compare2);
-    ll->printList();
+    ll->insertOrdered(s2, compare2);
+    ll->insertOrdered(s0, compare2);
+    ll->insertOrdered(s4, compare2);
+    ll->insertOrdered(s3, compare2);
+    ll->insertOrdered(s1, compare2);
+    //ll->insertOrdered(s0, compare2);
+    std::cout << ll->toString(toStringObj) << std::endl;
+
+    ll->clear();
+    std::cout << ll->toString(toStringObj) << std::endl;
 
     delete ll;
 }
