d92acdcfa2dc9385b154465aa36a6e978c7a30e4
Przemysław Kaźmierczak <przemyslawkazmierczak@przemysaws-air.home>
2025-12-03 18:44:00 +0100

    Final version

--- so/lab5.c
+++ so/lab5.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int validateStrtol(char* arg) {
+int validatedStoi(char* arg) {
     char *end;
     long num = strtol(arg, &end, 10);
     if (*end == '\0' && num >= 0 && num <= 100) {
@@ -13,52 +13,47 @@ int validateStrtol(char* arg) {
 
 char** fillArgs(int size, int firstIndex, char** argv) {
     char** args = malloc((size + 2) * sizeof(char*));
+    if (!args) return NULL;
 
     args[0] = argv[0];
     args[size + 1] = NULL;
 
-    for (int i = 0; i < size; i++) {
-        args[i + 1] = argv[firstIndex + i];
-    }
+    for (int i = 0; i < size; i++) { args[i + 1] = argv[firstIndex + i]; }
     return args;
 }
 
 void printArgs(char** args) {
-    int notFirst = 0;
-    (void)*args++;
+    args++;
 
+    int first = 1;
     printf("[");
     while (*args) {
-        if (notFirst) {
-            printf(", %s", *args++);
-        } else {
-            printf("%s", *args++);
-            notFirst = 1;
-        }
+        if (!first) { printf(", %s", *args++); }
+        else { printf("%s", *args++); first = 0; }
     }
     printf("]");
 }
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        fprintf(stderr, "Program wymaga min. 1 argument.\n");
+        fprintf(stderr, "The program requires at least one argument.\n");
         return 201;
     }
 
     for (int i = 1; i < argc; i++) {
-        if (validateStrtol(argv[i]) == -1) {
-            printf("Jeden z podanych argumentow nie jest liczba z przedzialu 0-100.\n");
+        if (validatedStoi(argv[i]) == -1) {
+            fprintf(stderr, "One of the arguments is not a number in the range [0-100].\n");
             return 202;
         }
     }
-
+    
     if (argc == 2) {
-        return validateStrtol(argv[1]);
+        return validatedStoi(argv[1]);
     }
 
     if (argc == 3) {
-        int arg1 = validateStrtol(argv[1]);
-        int arg2 = validateStrtol(argv[2]);
+        int arg1 = validatedStoi(argv[1]);
+        int arg2 = validatedStoi(argv[2]);
         return arg1 > arg2 ? arg1 : arg2;
     }
 
@@ -67,16 +62,17 @@ int main(int argc, char **argv) {
 
     char** argsL = fillArgs(sizeL, 1, argv);
     char** argsR = fillArgs(sizeR, sizeL + 1, argv);
+    if (!argsL || !argsR ) {
+        fprintf(stderr, "Malloc failed.");
+        free(argsL);
+        free(argsR);
+        return 203;
+    }
 
     pid_t id1 = fork();
-    if (id1 == 0) {
-        execv(argv[0], argsL);
-    }
-
+    if (id1 == 0) execv(argv[0], argsL);
     pid_t id2 = fork();
-    if (id2 == 0) {
-        execv(argv[0], argsR);
-    }
+    if (id2 == 0) execv(argv[0], argsR);
 
     pid_t parentId = getpid();
     int status1, child1 = waitpid(id1, &status1, 0);
@@ -93,9 +89,10 @@ int main(int argc, char **argv) {
     printf("\n");
 
     int maxStatus = (status1 > status2 ? status1 : status2);
-    printf("P: %d, Res: %d", parentId, (status1 > status2 ? status1 : status2));
-    printf("\n");
-    printf("\n");
+    printf("P: %d, Res: %d\n\n", parentId, maxStatus);
+
+    free(argsL);
+    free(argsR);
     
     return maxStatus;
 }
